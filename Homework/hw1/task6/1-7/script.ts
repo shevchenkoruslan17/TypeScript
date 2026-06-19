be499eccd79921fe14e604c6ac3b0246ba559a44
shevchenkoruslan17 <shevchenko.ruslan10@gmail.com>
2026-06-19 11:03:42 +0300

    homework task5-6

--- Homework/hw1/task6/1-7/script.ts
+++ Homework/hw1/task6/1-7/script.ts
@@ -1,16 +1,7 @@
 
-function sortNums(array:number[],direction:string):number[] | undefined{
-    if (direction === 'ascending') return array.sort((a, b) => a - b);
-    if (direction === 'descending') return array.sort((a, b) => b - a);
-}
-return array;
+function sortNums(arrayOfNums: number[], direction: string): number[] {
+    if (direction === 'ascending') return arrayOfNums.sort((a, b) => a - b);
+    if (direction === 'descending') return arrayOfNums.sort((a, b) => b - a);
 
-function sortNum(array:number[],direction:string):number[] | undefined {
-    const sort = array.sort((a, b) => a - b);
-    if( derection === 'ascending'){
-        return sort;
-    }
-    if ( direction === 'descending'){
-        return sort.reverse();
-    }
+    return arrayOfNums;
 }
